a71b14637bc53de45fa4944f583b638c53fb044e
unknown <smainali@dwnpcpu195-pc.dwnp.deerwalk.com>
2016-12-07 13:54:53 +0545

    object counting code shifted

--- assets/js/processImage.js
+++ assets/js/processImage.js
@@ -3,10 +3,10 @@
  */
 
 var pixelToMeter = 0.3679;
+var usedLabel = [],polygonPoints = [];
+var parentChild = {},matrix = {};
 
 function startProcess(polyXPoints,polyYPoints){
-
-    var polygonPoints = [];
     for(var k=0;k<polyXPoints.length;k++){
         var poly =[];
         poly.push(polyXPoints[k]);
@@ -16,18 +16,10 @@ function startProcess(polyXPoints,polyYPoints){
 
     var canvas = document.getElementById("destinationCanvas");
     var canvasContext = canvas.getContext('2d');
-
     var imgPixels = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
-    var pCount = 0;
-    var neighBorPixels = [];
-    neighBorPixels.push([-1,-1]);
-    neighBorPixels.push([0,-1]);
-    neighBorPixels.push([1,-1]);
-    neighBorPixels.push([-1,1]);
-    neighBorPixels.push([1,0]);
-    neighBorPixels.push([-1,1]);
-    neighBorPixels.push([0,1]);
-    neighBorPixels.push([1,1]);
+
+    var neighBorPixels = [[-1,-1],[0,-1],[1,-1],[-1,1],[1,0],[-1,1],[0,1],[1,1]];
+
 
     for(var y = 0; y < imgPixels.height; y++){
         for(var x = 0; x < imgPixels.width; x++){
@@ -35,7 +27,6 @@ function startProcess(polyXPoints,polyYPoints){
             if(inside(point,polygonPoints)){
                 var i = (y * 4) * imgPixels.width + x * 4;
                 var avg = Math.round((imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3);
-                console.log(avg);
                 if(avg==239){
                     var goodNeighbor = true;
                     for(var n=0;n<neighBorPixels.length;n++){
@@ -49,16 +40,167 @@ function startProcess(polyXPoints,polyYPoints){
                         }
                     }
                     if(goodNeighbor){
-                        pCount++;
+                        matrix[x + "," + y] = 1;
+                    }else{
+                        matrix[x + "," + y] = 0;
                     }
+                }
+            }else{
+                matrix[x + "," + y] = 0;
+            }
+        }
+    }
+    canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+    $("#originalImage").attr("src",canvas.toDataURL("image/png"));
+    $("#totalBuildings").val(getBuildingCount(canvas));
+    $("#destinationCanvas").addClass("hide");
+    $("#sourceCanvas").removeClass("hide");
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+function getBuildingCount(canvas) {
+
+    var canvasContext = canvas.getContext('2d');
+    var imgPixels = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
+
+    var imageAlgo = new ImageAlgorithm(imgPixels.width,imgPixels.height,matrix);
+    var erosionMatrix = imageAlgo.erosion();
+    var dilationMatrix = imageAlgo.dilation(erosionMatrix);
+    var pCount = 0;
+
+    for (var y = 0; y < imgPixels.height; y++) {
+        for (var x = 0; x < imgPixels.width; x++) {
+            var i = (y * 4) * imgPixels.width + x * 4;
+            if(dilationMatrix[x + "," + y] == 1){
+                imgPixels.data[i] = 255;
+                imgPixels.data[i + 1] = 255;
+                imgPixels.data[i + 2] = 255;
+                matrix[x + "," + y] = 1;
+                pCount++
+            } else {
+                imgPixels.data[i] = 0;
+                imgPixels.data[i + 1] = 0;
+                imgPixels.data[i + 2] = 0;
+                matrix[x + "," + y] = 0;
+            }
+        }
+    }
+
+    for(var y = 0; y < imgPixels.height; y++) {
+        for (var x = 0; x < imgPixels.width; x++) {
+            if (matrix[x+","+y] == 1) {
+                var first = getMatrixVal(x - 1, y - 1);
+                var second = getMatrixVal(x, y - 1);//top
+                var third = getMatrixVal(x + 1, y - 1);
+                var fourth = getMatrixVal(x - 1, y);//left
+                var fifth = getMatrixVal(x + 1, y);
+                var sixth = getMatrixVal(x - 1, y + 1);
+                var seventh = getMatrixVal(x, y + 1);
+                var eighth = getMatrixVal(x + 1, y + 1);
+
+                if (first === 0 && second === 0 && third === 0 && fourth === 0 && fifth === 0 && sixth === 0 && seventh === 0
+                    && eighth === 0) {
+                    matrix[x+","+y] = 0;
+                }
+                else {
+                    matrix[x+","+y] = getLabel(x, y);
                 }
             }
         }
     }
+    Object.keys(parentChild).forEach(function (key) {
+        var childList = parentChild[key];
+        var otherKeys = Object.keys(parentChild);
+        otherKeys.forEach(function (k) {
+            if(key!=k){
+                if(itemExist(k, childList))
+                    delete parentChild[k];
+            }
+        });
+
+    });
+
     $("#resultRow").show();
     displayResult(pCount*pixelToMeter);
     canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-    $("#originalImage").attr("src",canvas.toDataURL("image/png"))
+    $("#processedImage").attr("src", canvas.toDataURL("image/png"));
+    return Object.keys(parentChild).length;
+}
+
+function itemExist(item,list){
+    if(list===undefined){
+        return false
+    }
+    for(var i=0;i<list.length;i++){
+        if(item==list[i]){
+            return true
+        }
+    };
+    return false
+}
+
+function getLabel(x,y){
+    var left = getMatrixVal(x-1,y);
+    var top = getMatrixVal(x,y-1);
+
+    if(itemExist(left, usedLabel) && itemExist(top, usedLabel)){
+        var parent,child;
+        if(top>=left){
+            parent = left;
+            child = top;
+        } else{
+            parent = top;
+            child = left;
+        }
+        if(parent in parentChild) {
+            var childList = parentChild[parent];
+            if (!(itemExist(child, childList))) {
+                childList.push(child);
+                parentChild[parent] = childList;
+                return parent;
+            }
+        }else{
+            var childList;
+            for (var key in parentChild) {
+                if (parentChild.hasOwnProperty(key)) {
+                    childList = parentChild[key];
+                    if (itemExist(parent, childList)) {
+                        if (!(itemExist(child, childList))) {
+                            childList.push(child);
+                            parentChild[key] = childList;
+                        }
+                        return parent;
+                    }
+                }
+            }
+            childList = [];
+            childList.push(child);
+            parentChild[parent] = childList;
+        }
+        return parent
+    }
+    if(jQuery.inArray(left, usedLabel) !== -1){
+        return left;
+    }
+    if(jQuery.inArray(top, usedLabel) !== -1){
+        return top;
+    }
+    if(usedLabel.length==0){
+        usedLabel.push(2);
+        return 2;
+    }
+    var newLabel = usedLabel[usedLabel.length-1]+1;
+    usedLabel.push(newLabel);
+    return newLabel;
+}
+
+function getMatrixVal(x,y){
+    try{
+        return matrix[x+","+y]
+    }catch (err){
+        return 0
+    }
 }
 
 function inside(point, vs) {
@@ -79,8 +221,6 @@ function inside(point, vs) {
 
     return inside;
 }
-
-
 
 function displayResult(area){
     $("#totalArea").val(area);
@@ -105,4 +245,12 @@ function changeVals(area){
     for(var i=0;i<monthWise.length;i++){
         $("input[id='"+i+"'").val((monthWise[i]*energy).toFixed(2))
     }
+}
+
+function disposeAllVariable(){
+    usedLabel = [];
+    polygonPoints = [];
+    parentChild = {};
+    matrix = {};
+    console.log("Cleared all used resources.")
 }
