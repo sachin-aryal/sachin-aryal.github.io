d63ccfe61772b6eb3199e2e76bf5b2510cfa9c7f
sanzip <sanjeev.mainali@deerwalk.edu.np>
2016-12-28 00:23:44 +0545

    moved all the codes

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,6 +1,35 @@
 /**
  * Created by iam on 12/6/16.
  */
+var geocoder;
+
+var lat = 85.32247;
+var lon = 27.68248;
+
+function codeAddress() {
+   geocoder = new google.maps.Geocoder();
+    var address = document.getElementById("address").value;
+    geocoder.geocode( { 'address': address, 'region': 'np'}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var inp=results[0].geometry.location;
+
+		var lat = results[0].geometry.location.lat();
+		var lon = results[0].geometry.location.lng();
+
+          console.log(address + " " + lat + " " + lon) ;
+
+          map.setView(new ol.View({
+              center:  ol.proj.transform([parseFloat(lon), parseFloat(lat)],
+                  'EPSG:4326', 'EPSG:900913'),
+              zoom: map.getView().getZoom()
+          }));
+
+      } else {
+        alert("The searched location was not found.");
+      }
+    });
+}
+
 
 var polyXPoints = [],polyYPoints=[];
 
@@ -28,12 +57,6 @@ var map = new ol.Map({
             source: new ol.source.OSM({
                 "url" : "http://tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"
             })
-        }),
-        new ol.layer.Vector({
-            source: new ol.source.Vector({
-                url: 'data/geojson/countries.geojson',
-                format: new ol.format.GeoJSON()
-            })
         })
     ],
     target: 'basicMap',
@@ -43,12 +66,12 @@ var map = new ol.Map({
         })
     }),
     view: new ol.View({
-       //center: ol.proj.transform([85.32247, 27.68248], 'EPSG:4326', 'EPSG:900913'),//default
-		 center: ol.proj.transform([85.3398418, 27.7095784], 'EPSG:4326', 'EPSG:900913'),//DEERWALK
+        center: ol.proj.transform([lat, lon], 'EPSG:4326', 'EPSG:900913'),
+        maxZoom:19,
+        minZoom: 17,
         zoom: 18,
         projection: 'EPSG:3857'
     })
-
 });
 
 var vectorSource = new ol.source.Vector();
@@ -62,7 +85,7 @@ map.addLayer(vectorLayer);
 var startDraw = document.getElementById('drawPoly');
 var resetDraw = document.getElementById('resetMap');
 startDraw.addEventListener('click', function(e) {
-    
+    $("#processButton").prop('disabled', false);
     var first=0;
     map.once('postcompose', function(event) {
         var canvas = event.context.canvas;
@@ -105,16 +128,44 @@ startDraw.addEventListener('click', function(e) {
 }, false);
 
 resetDraw.addEventListener('click', function(e) {
+    $("#resultRow").hide();
     $("#destinationCanvas").addClass("hide");
     $("#sourceCanvas").removeClass("hide");
     polyXPoints=[];
     polyYPoints=[];
+    $("#processButton").prop('disabled', true);
 });
 
 
 function uploadImageData(){
+    if(polyXPoints.length == 0 && polyYPoints == 0){
+        initializeInitial()
+    }
     startProcess(polyXPoints,polyYPoints);
     disposeAllVariable();
     polyXPoints = [];
     polyYPoints=[];
+}
+
+function initializeInitial(){
+    console.log("Initializing Points with default values.");
+    var canvas = document.getElementById("destinationCanvas");
+    var cWidth = canvas.width;
+    var cHeight = canvas.height;
+    polyXPoints = [0,cWidth,cWidth,0];
+    polyYPoints = [0,0,cHeight,cHeight];
+}
+
+$("#myBtn").click(function(){
+    $("#myModal").modal();
+});
+$("#resultRow").hide();
+$("#processButton").prop('disabled', true);
+
+
+function startTour() {
+    var tour = introJs();
+    tour.setOption('tooltipPosition', 'auto');
+    tour.setOption('positionPrecedence', ['left', 'right', 'bottom', 'top'])
+    tour.start()
 }
